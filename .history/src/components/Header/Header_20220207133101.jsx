0f28554d60c9feafe71bd261eac8ef6df43fa08a
GokulDev4U <sreeharipisharody678@gmail.com>
2022-02-07 15:33:21 +0530

    final changes

---
+++ .history/src/components/Header/Header_20220207133101.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './Header.modular.css';
+const Header = () => {
+  return <div className={styles.container}>
+      <p className={styles.heading}></p>
+  </div>;
+};
+
+export default Header;
